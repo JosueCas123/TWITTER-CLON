6072484851f362f6d3225c35f44eb037113edf00
Josue Castillo <isaac9900.jc@gmail.com>
2024-06-06 12:04:43 -0400

    3-delete post added

--- frontend/src/components/common/Sidebar.tsx
+++ frontend/src/components/common/Sidebar.tsx
@@ -10,6 +10,8 @@ import toast from "react-hot-toast";
 
 const Sidebar = () => {
 	const queryClient = useQueryClient()
+
+	
 	const {mutate:logout} = useMutation({
 		mutationFn: async () => {
 			try {
